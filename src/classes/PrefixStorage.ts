b4c69c48f5fa477b9b4a4bc56809a507445095e0
Pavol Hejný <me@pavolhejny.com>
2020-08-26 18:48:11 +0200

    Prefix separators

--- src/classes/PrefixStorage.ts
+++ src/classes/PrefixStorage.ts
@@ -5,7 +5,11 @@ import { Awaitable } from '../interfaces/utils/Awaitable';
  * This class behaves like LocalStorage but separates keys by prefix
  */
 export class PrefixStorage<T> implements IStorage<T> {
-    constructor(private baseStorage: IStorage<T>, private keyPrefix: string) {}
+    constructor(
+        private baseStorage: IStorage<T>,
+        private keyPrefix: string,
+        private separator: string = '_',
+    ) {}
 
     /**
      * Returns the number of key/value pairs currently present in the list associated with the object.
@@ -25,7 +29,7 @@ export class PrefixStorage<T> implements IStorage<T> {
      * Returns the current value associated with the given key, or null if the given key does not exist in the list associated with the object.
      */
     public getItem(key: string): Awaitable<T | null> {
-        return this.baseStorage.getItem(this.keyPrefix + key);
+        return this.baseStorage.getItem(this.keyPrefix + this.separator + key);
     }
 
     /**
@@ -40,13 +44,13 @@ export class PrefixStorage<T> implements IStorage<T> {
      * Removes the key/value pair with the given key from the list associated with the object, if a key/value pair with the given key exists.
      */
     public removeItem(key: string): void {
-        this.baseStorage.removeItem(this.keyPrefix + key);
+        this.baseStorage.removeItem(this.keyPrefix + this.separator + key);
     }
 
     /**
      * Sets the value of the pair identified by key to value, creating a new key/value pair if none existed for key previously.
      */
     public setItem(key: string, value: T): Awaitable<void> {
-        this.baseStorage.setItem(this.keyPrefix + key, value);
+        this.baseStorage.setItem(this.keyPrefix + this.separator + key, value);
     }
 }
