6d2419d8f210e82248557e05d2c14e7162d0631c
Pavol Hejný <me@pavolhejny.com>
2020-09-02 21:05:26 +0200

    Value in IObservableStorage

--- src/interfaces/IObservableStorage.ts
+++ src/interfaces/IObservableStorage.ts
@@ -4,6 +4,7 @@ export type IValue = Record<string, string | number | null | undefined>;
 
 /** This is simmilar to Web Storage API interface but values are observable streams. */
 export interface IObservableStorage<TValue extends IValue> {
+    value: TValue;
     values: Observable<TValue>;
     pushValue(value: TValue): void;
     dispose(): void;
