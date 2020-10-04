281300ff965ca947a64b493a2a0df7142a36ed49
Pavol Hejny <me@pavolhejny.com>
2020-10-04 15:32:55 +0200

    ISerialized wider support

--- src/interfaces/ISerialized.ts
+++ src/interfaces/ISerialized.ts
@@ -1,6 +1,5 @@
-export type ISerialized = ISerializedObject | string;
-
-// TODO: Split into multiple exported interfaces
+// TODO: DRY
+export type ISerialized = ISerializedObject | ISerialized[] | string | number | null | undefined;
 
 // TODO: This may be generically typed with deserialized type for example ISerialized<Vector>
 interface ISerializedObject /* extends IJson*/ {
