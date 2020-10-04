3582afc7d9f96a99a94a77695f59c21787f90c0c
Pavol Hejny <me@pavolhejny.com>
2020-10-04 15:33:07 +0200

    Removing changes

--- src/interfaces/ISerialized.ts
+++ src/interfaces/ISerialized.ts
@@ -1,5 +1,6 @@
-// TODO: DRY
-export type ISerialized = ISerializedObject | ISerialized[] | string | number | null | undefined;
+export type ISerialized = ISerializedObject | string;
+
+// TODO: Split into multiple exported interfaces
 
 // TODO: This may be generically typed with deserialized type for example ISerialized<Vector>
 interface ISerializedObject /* extends IJson*/ {
