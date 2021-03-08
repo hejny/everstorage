77d2df241e30d74b66c1364ba7fdc0e249f72607
Pavol Hejny <me@pavolhejny.com>
2021-03-08 01:39:09 +0100

    IObservableStorage extends IDestroyable

--- src/interfaces/IObservableStorage.ts
+++ src/interfaces/IObservableStorage.ts
@@ -1,11 +1,11 @@
 import { Observable } from 'rxjs';
-
+import { IDestroyable } from './IDestroyable';
 import { ISerializable } from './ISerializable';
 
 /** This is simmilar to Web Storage API interface but values are observable streams. */
-export interface IObservableStorage<TValue extends ISerializable> {
+export interface IObservableStorage<TValue extends ISerializable>
+    extends IDestroyable {
     value: TValue;
     values: Observable<TValue>;
     pushValue(value: TValue): void;
-    destroy(): void;
 }
