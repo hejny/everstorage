ff9880611a5f66bcecb589a73230043aee260bd5
Pavol Hejny <me@pavolhejny.com>
2023-07-22 14:35:09 +0200

    Fix  Duplicate identifier 'IJson'.

--- src/index.ts
+++ src/index.ts
@@ -2,69 +2,65 @@
 // ⚠️ Warning: Do not edit by hand, all changes will be lost on next execution!
 
 import { AsyncStorage } from './classes/AsyncStorage';
-import { BlackholeStorage } from './classes/BlackholeStorage';
-import { blackholeStorage } from './classes/BlackholeStorage';
+import { BlackholeStorage, blackholeStorage } from './classes/BlackholeStorage';
 import { BrowserHistoryPathHashStorage } from './classes/BrowserHistoryStorage/BrowserHistoryPathHashStorage';
 import { BrowserHistoryPathStorage } from './classes/BrowserHistoryStorage/BrowserHistoryPathStorage';
 import { BrowserHistoryQueryStorage } from './classes/BrowserHistoryStorage/BrowserHistoryQueryStorage';
 import { BrowserHistoryUrlStorage } from './classes/BrowserHistoryStorage/BrowserHistoryUrlStorage';
 import { BrowserHistoryUrlStringStorage } from './classes/BrowserHistoryStorage/BrowserHistoryUrlStringStorage';
-import { IBrowserHistoryStorageOptions } from './classes/BrowserHistoryStorage/IBrowserHistoryStorageOptions';
-import { BROWSER_HISTORY_STORAGE_OPTIONS_DEFAULTS } from './classes/BrowserHistoryStorage/IBrowserHistoryStorageOptions';
+import {
+    BROWSER_HISTORY_STORAGE_OPTIONS_DEFAULTS,
+    IBrowserHistoryStorageOptions,
+} from './classes/BrowserHistoryStorage/IBrowserHistoryStorageOptions';
 import { MemoryStorage } from './classes/MemoryStorage';
 import { MultiStorage } from './classes/MultiStorage';
-import { ObjectStorage } from './classes/ObjectStorage';
-import { objectLocalStorage } from './classes/ObjectStorage';
+import { objectLocalStorage, ObjectStorage } from './classes/ObjectStorage';
 import { PrefixStorage } from './classes/PrefixStorage';
 import { SerializedStorage } from './classes/SerializedStorage';
 import { createEmptyValue } from './interfaces/createEmptyValue';
 import { IBrowserState } from './interfaces/IBrowserState';
 import { IInstantiable } from './interfaces/IInstantiable';
 import { IJson } from './interfaces/IJson';
-import { IJson } from './interfaces/IJson';
 import { IObservableStorage } from './interfaces/IObservableStorage';
-import { ISerializable } from './interfaces/ISerializable';
-import { ISerializableCore } from './interfaces/ISerializable';
+import { ISerializable, ISerializableCore } from './interfaces/ISerializable';
 import { ISerialized } from './interfaces/ISerialized';
 import { ISerializeRule } from './interfaces/ISerializeRule';
 import { IStorage } from './interfaces/IStorage';
 import { createUniqueIdentifierFromParams } from './utils/createUniqueIdentifierFromParams';
 import { isNumeric } from './utils/isNumeric';
 import { Serializer } from './utils/Serializer';
-import { serializer } from './utils/serializers';
-import { serializerWithDate } from './utils/serializers';
+import { serializer, serializerWithDate } from './utils/serializers';
 
 export {
-IJson,
-IJson,
-IStorage,
-isNumeric,
-serializer,
-Serializer,
-ISerialized,
-MultiStorage,
-AsyncStorage,
-ISerializable,
-IInstantiable,
-IBrowserState,
-PrefixStorage,
-ObjectStorage,
-MemoryStorage,
-ISerializeRule,
-createEmptyValue,
-blackholeStorage,
-BlackholeStorage,
-ISerializableCore,
-SerializedStorage,
-serializerWithDate,
-IObservableStorage,
-objectLocalStorage,
-BrowserHistoryUrlStorage,
-BrowserHistoryPathStorage,
-BrowserHistoryQueryStorage,
-IBrowserHistoryStorageOptions,
-BrowserHistoryPathHashStorage,
-BrowserHistoryUrlStringStorage,
-createUniqueIdentifierFromParams,
-BROWSER_HISTORY_STORAGE_OPTIONS_DEFAULTS
+    AsyncStorage,
+    blackholeStorage,
+    BlackholeStorage,
+    BROWSER_HISTORY_STORAGE_OPTIONS_DEFAULTS,
+    BrowserHistoryPathHashStorage,
+    BrowserHistoryPathStorage,
+    BrowserHistoryQueryStorage,
+    BrowserHistoryUrlStorage,
+    BrowserHistoryUrlStringStorage,
+    createEmptyValue,
+    createUniqueIdentifierFromParams,
+    IBrowserHistoryStorageOptions,
+    IBrowserState,
+    IInstantiable,
+    IJson,
+    IObservableStorage,
+    ISerializable,
+    ISerializableCore,
+    ISerialized,
+    ISerializeRule,
+    isNumeric,
+    IStorage,
+    MemoryStorage,
+    MultiStorage,
+    objectLocalStorage,
+    ObjectStorage,
+    PrefixStorage,
+    SerializedStorage,
+    serializer,
+    Serializer,
+    serializerWithDate,
 };
--- src/interfaces/IJson.ts
+++ src/interfaces/IJson.ts
@@ -1,19 +1,6 @@
 import { JsonValue } from 'type-fest';
 
 export type IJson = JsonValue;
-/*
-export interface IJson {
-    [key: string]:
-        | string
-        | number
-        | boolean
-        | null
-        | undefined
-        | IJson[]
-        | IJson;
-}
-
-*/
 
 /**
  * TODO: !!! Just ACRY change IJson to JsonValue and remove this file
