44cad5e6d7f288453432001e97d4d7bb57340684
Pavol Hejny <me@pavolhejny.com>
2024-08-11 12:14:03 +0200

    🧹 Organize imports
    🔼 This commit was automatically generated by [Batch project editor](https://github.com/hejny/batch-project-editor) using workflow organizeImports

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,16 @@
 // ⚠️ Warning: Do not edit by hand, all changes will be lost on next execution!
 
 import { AsyncStorage } from './classes/AsyncStorage';
-import { BlackholeStorage } from './classes/BlackholeStorage';
-import { blackholeStorage } from './classes/BlackholeStorage';
+import { BlackholeStorage, blackholeStorage } from './classes/BlackholeStorage';
 import { BrowserHistoryPathHashStorage } from './classes/BrowserHistoryStorage/BrowserHistoryPathHashStorage';
 import { BrowserHistoryPathStorage } from './classes/BrowserHistoryStorage/BrowserHistoryPathStorage';
 import { BrowserHistoryQueryStorage } from './classes/BrowserHistoryStorage/BrowserHistoryQueryStorage';
 import { BrowserHistoryUrlStorage } from './classes/BrowserHistoryStorage/BrowserHistoryUrlStorage';
 import { BrowserHistoryUrlStringStorage } from './classes/BrowserHistoryStorage/BrowserHistoryUrlStringStorage';
-import { IBrowserHistoryStorageOptions } from './classes/BrowserHistoryStorage/IBrowserHistoryStorageOptions';
-import { BROWSER_HISTORY_STORAGE_OPTIONS_DEFAULTS } from './classes/BrowserHistoryStorage/IBrowserHistoryStorageOptions';
+import { BROWSER_HISTORY_STORAGE_OPTIONS_DEFAULTS, IBrowserHistoryStorageOptions } from './classes/BrowserHistoryStorage/IBrowserHistoryStorageOptions';
 import { MemoryStorage } from './classes/MemoryStorage';
 import { MultiStorage } from './classes/MultiStorage';
-import { ObjectStorage } from './classes/ObjectStorage';
-import { objectLocalStorage } from './classes/ObjectStorage';
+import { objectLocalStorage, ObjectStorage } from './classes/ObjectStorage';
 import { PrefixStorage } from './classes/PrefixStorage';
 import { SerializedStorage } from './classes/SerializedStorage';
 import { createEmptyValue } from './interfaces/createEmptyValue';
@@ -22,47 +19,45 @@ import { IBrowserState } from './interfaces/IBrowserState';
 import { IInstantiable } from './interfaces/IInstantiable';
 import { IJson } from './interfaces/IJson';
 import { IObservableStorage } from './interfaces/IObservableStorage';
-import { ISerializable } from './interfaces/ISerializable';
-import { ISerializableCore } from './interfaces/ISerializable';
+import { ISerializable, ISerializableCore } from './interfaces/ISerializable';
 import { ISerialized } from './interfaces/ISerialized';
 import { ISerializeRule } from './interfaces/ISerializeRule';
 import { IStorage } from './interfaces/IStorage';
 import { createUniqueIdentifierFromParams } from './utils/createUniqueIdentifierFromParams';
 import { isNumeric } from './utils/isNumeric';
 import { Serializer } from './utils/Serializer';
-import { serializer } from './utils/serializers';
-import { serializerWithDate } from './utils/serializers';
+import { serializer, serializerWithDate } from './utils/serializers';
 
 export {
-IJson,
-IStorage,
-isNumeric,
-serializer,
-Serializer,
-ISerialized,
-MultiStorage,
-AsyncStorage,
-ISerializable,
-IInstantiable,
-IBrowserState,
-PrefixStorage,
-ObjectStorage,
-MemoryStorage,
-ISerializeRule,
-createEmptyValue,
-blackholeStorage,
-BlackholeStorage,
-ISerializableCore,
-SerializedStorage,
-serializerWithDate,
-IObservableStorage,
-objectLocalStorage,
-BrowserHistoryUrlStorage,
-BrowserHistoryPathStorage,
-BrowserHistoryQueryStorage,
-IBrowserHistoryStorageOptions,
-BrowserHistoryPathHashStorage,
-BrowserHistoryUrlStringStorage,
-createUniqueIdentifierFromParams,
-BROWSER_HISTORY_STORAGE_OPTIONS_DEFAULTS
+  IJson,
+  IStorage,
+  isNumeric,
+  serializer,
+  Serializer,
+  ISerialized,
+  MultiStorage,
+  AsyncStorage,
+  ISerializable,
+  IInstantiable,
+  IBrowserState,
+  PrefixStorage,
+  ObjectStorage,
+  MemoryStorage,
+  ISerializeRule,
+  createEmptyValue,
+  blackholeStorage,
+  BlackholeStorage,
+  ISerializableCore,
+  SerializedStorage,
+  serializerWithDate,
+  IObservableStorage,
+  objectLocalStorage,
+  BrowserHistoryUrlStorage,
+  BrowserHistoryPathStorage,
+  BrowserHistoryQueryStorage,
+  IBrowserHistoryStorageOptions,
+  BrowserHistoryPathHashStorage,
+  BrowserHistoryUrlStringStorage,
+  createUniqueIdentifierFromParams,
+  BROWSER_HISTORY_STORAGE_OPTIONS_DEFAULTS
 };
